68ae038a504fcad12dd6400aa9a24c69f3db0de0
Sônia Ribeiro <soniasoimoveis@gmail.com>
2023-01-08 20:25:36 -0300

    Projetos criados para conclusão dos exercicios do dia 07/01

---
+++ IdentificadorDeIdade/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.Serialization.Formatters;
+using System.Linq;
+
+namespace IdentificadorDeIdade
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("##################################");
+            Console.WriteLine("###Identificação de Senioridade###");
+            Console.WriteLine("##################################");
+
+            String[] nomes = new string[3];
+            List<int> idades = new List<int>();
+            
+
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.WriteLine("Informe o nome da pessoa: ");
+                nomes[i] = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("Informe a idade dessa pessoa: ");
+                idades.Add(Convert.ToInt32(Console.ReadLine()));
+
+            }
+
+
+            int maiorIdade = idades.Max();
+
+            int indexListaIdades = idades.IndexOf(maiorIdade);
+
+            Console.WriteLine($"A pessoa mais velha digitada é: {nomes[indexListaIdades]}");
+                                
+            Console.ReadKey();
+        }
+
+    }
+
+}
---
+++ SegregacaoDeNumerosPositivos/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualBasic;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+
+
+namespace SegregacaoDeNumerosPositivos
+{
+    internal class Program
+    {
+
+        //2.    Faça um algoritmo que receba 15 números e exiba somente os positivos para o usuário
+        static void Main(string[] args)
+        {
+            Console.WriteLine("#####################################");
+            Console.WriteLine("###Segragação de Números Positivos###");
+            Console.WriteLine("#####################################");
+
+            List<int> positivo = new List<int>();         
+            int[] numeros = new int[5];
+           
+            int x = 0;
+            for (int i = 0; i < numeros.Length; i++)
+            {   
+                Console.WriteLine("Digite 5 números de sua escolha: ");
+                int numero = Convert.ToInt32(Console.ReadLine());
+
+                numeros[i] = numero;
+
+                if (numero >= 0)
+                {
+              
+                    positivo.Add(numero);
+                    x = x + 1;  
+                }                
+            }
+
+            Console.WriteLine("Os números digitados foram: ");
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine(numeros[i]);                
+            }
+
+            Console.WriteLine("Dentre eles os positivos são: ");
+
+            for (int i = 0; i < positivo.Count; i++)
+            {
+                Console.WriteLine(positivo[i]);
+            }
+
+
+        }
+    }
+}
