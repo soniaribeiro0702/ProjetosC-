996d095d2bd718cf4f29904dc6fce7f99667ba63
Sônia Ribeiro <soniasoimoveis@gmail.com>
2023-01-09 20:02:39 -0300

    Refatoração de código e divisão de classe.

--- IdentificadorDeIdade/FluxoPessoa.cs
+++ IdentificadorDeIdade/FluxoPessoa.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace IdentificadorDeIdade
 {
-    internal class FluxoPessoa
+    public class FluxoPessoa
     {
+        public String[] nomes = new string[3];
+        public List<int> idades = new List<int>();
+        public void Executar()
+        {
+            Titulo();
+            Condicional();
+            Resultado();
+        }
+        private void Titulo()
+        {
+            Console.WriteLine("##################################");
+            Console.WriteLine("###Identificação de Senioridade###");
+            Console.WriteLine("##################################");
+        }
+
+        private void Condicional()
+        {
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.WriteLine("Informe o nome da pessoa: ");
+                nomes[i] = (Console.ReadLine());
+                Console.WriteLine("Informe a idade dessa pessoa: ");
+                idades.Add(Convert.ToInt32(Console.ReadLine()));
+
+            }
+        }
+
+        private void Resultado()
+        {
+            int maiorIdade = idades.Max();
+            int indexListaIdades = idades.IndexOf(maiorIdade);
+            Console.WriteLine($"A pessoa mais velha digitada é: {nomes[indexListaIdades]}");
+
+            Console.ReadKey();
+        }
     }
 }
