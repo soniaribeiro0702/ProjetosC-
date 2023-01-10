25734eee55e206d7653995b6a9c3d00b0c83ca3e
Sônia Ribeiro <soniasoimoveis@gmail.com>
2023-01-09 21:09:55 -0300

    Melhorias no código de Segregação de números positivos.

--- SegregacaoDeNumerosPositivos/Program.cs
+++ SegregacaoDeNumerosPositivos/Program.cs
@@ -12,44 +12,10 @@ namespace SegregacaoDeNumerosPositivos
         //2.    Faça um algoritmo que receba 15 números e exiba somente os positivos para o usuário
         static void Main(string[] args)
         {
-            Console.WriteLine("#####################################");
-            Console.WriteLine("###Segragação de Números Positivos###");
-            Console.WriteLine("#####################################");
+            var numeroPositivos = new FluxoDeNumeros();
+            numeroPositivos.Executar();
 
-            List<int> positivo = new List<int>();         
-            int[] numeros = new int[5];
-           
-            int x = 0;
-            for (int i = 0; i < numeros.Length; i++)
-            {   
-                Console.WriteLine("Digite 5 números de sua escolha: ");
-                int numero = Convert.ToInt32(Console.ReadLine());
-
-                numeros[i] = numero;
-
-                if (numero >= 0)
-                {
-              
-                    positivo.Add(numero);
-                    x = x + 1;  
-                }                
-            }
-
-            Console.WriteLine("Os números digitados foram: ");
-
-            for (int i = 0; i < numeros.Length; i++)
-            {
-                Console.WriteLine(numeros[i]);                
-            }
-
-            Console.WriteLine("Dentre eles os positivos são: ");
-
-            for (int i = 0; i < positivo.Count; i++)
-            {
-                Console.WriteLine(positivo[i]);
-            }
-
-
+            Console.ReadKey();
         }
     }
 }
